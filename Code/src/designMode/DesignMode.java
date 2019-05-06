f4c17b70568094725dfbf84591e13493ca84d64e
周子杰(樱庭) <zjzhou@uuzu.com>
2019-05-06 18:28:59 +0800

    19-05-06 添加新内容

--- Code/src/designMode/DesignMode.java
+++ Code/src/designMode/DesignMode.java
@@ -2,17 +2,13 @@ package designMode;
 
 public class DesignMode {
 	public static void main(String []args) {
-		String exp = "2+8";
-		StrategyICalculator cal = new StrategyPlus();
-		int result = cal.calculator(exp);
-		System.out.println("2+8="+result);
-		exp = "8-2";
-		cal = new StrategyMinus();
-		result = cal.calculator(exp);
-		System.out.println("8-2="+result);
-		exp = "8*2";
-		cal = new StrategyMultiply();
-		result = cal.calculator(exp);
-		System.out.println("8*2="+result);
+		MyHandler h1 = new MyHandler("h1");
+		MyHandler h2 = new MyHandler("h2");
+		MyHandler h3 = new MyHandler("h3");
+
+		h1.setHandler(h2);
+		h2.setHandler(h3);
+
+		h1.operator();
 	}
 }
